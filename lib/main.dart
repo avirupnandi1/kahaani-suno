eb17935eb436cf299a01032944112fb3a3cc399a
Avirup Nandi <106444983+avirupnandi1@users.noreply.github.com>
2024-11-04 21:48:36 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Story Suno',
+      title: 'Kahaani Suno',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 252, 35, 35)),
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const MyHomePage(title: 'Story Suno'),
+      home: const MyHomePage(title: 'Kahaani Suno'),
     );
   }
 }
